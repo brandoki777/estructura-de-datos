b2908c76c289e480afde1fb27b43ec4f889c97ea
brandoki777 <bransanro4789@gmail.com>
2026-03-02 03:02:23 -0500

    Create ejercicio7.java

---
+++ ejercicio7.java
@@ -0,0 +1,18 @@
+
+public class ejercicio7 {
+    public static void main(String[] args) {
+        int [] Almacen = {10,25,32,48,51};
+        int [] Reportes= Almacen;
+        Reportes[2] = Reportes[2] - 10;
+        System.out.println("Stock desde stockAlmacen:");
+        for (int i = 0; i < Almacen.length; i++) {
+            System.out.println("Producto " + i + ": " + Almacen[i]);
+        }
+        System.out.println("\nStock desde stockReportes:");
+        for (int i = 0; i < Reportes.length; i++) {
+            System.out.println("Producto " + i + ": " + Reportes[i]);
+        }
+
+    }
+    
+}
