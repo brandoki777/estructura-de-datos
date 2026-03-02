18b30425a6a5b648dec2a36ff2bd32c0e74863da
brandoki777 <bransanro4789@gmail.com>
2026-03-02 03:02:27 -0500

    Create ejercicio8.java

---
+++ ejercicio8.java
@@ -0,0 +1,13 @@
+
+
+public class ejercicio8 {
+    public static void main(String[] args) {
+        String UltimoMensaje = "hola";
+        String CopiaTexto= UltimoMensaje;
+        CopiaTexto="como estas?";
+        System.out.println("Ultimo mensaje: "+UltimoMensaje);
+        System.out.println("Copia de texto: "+CopiaTexto);
+        
+
+    }
+}
