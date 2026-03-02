507c10a32864bd898660c56128fbc12fc1e09393
brandoki777 <bransanro4789@gmail.com>
2026-03-02 03:02:15 -0500

    Create ejercicio3.java

---
+++ ejercicio3.java
@@ -0,0 +1,22 @@
+
+public class ejercicio3 {
+    public static void main(String[] args) {
+        
+        double Temperatura = 81.0;
+        String Sensor = "DH111";
+        int numerolecturas = 5;
+        boolean alarma = false;
+
+        if (Temperatura>80){
+            alarma = true;
+        }
+        System.out.println("-------------------Datos del sensor de temperatura-------------------" );
+        System.out.println("Temperatura: " + Temperatura);
+        System.out.println("Sensor: " + Sensor);
+        System.out.println("Numero de lecturas: " + numerolecturas);
+        System.out.println("Alarma activada: " + alarma);
+        System.out.println("----------------------------------------------------------------------")
+
+    }
+
+}
