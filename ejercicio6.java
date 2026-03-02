7035a4d932760a88008c0501a4d4bfd915423b17
brandoki777 <bransanro4789@gmail.com>
2026-03-02 03:02:22 -0500

    Create ejercicio6.java

---
+++ ejercicio6.java
@@ -0,0 +1,14 @@
+
+public class ejercicio6 {
+    public static void main(String[] args) {
+        int turnoActual=24;
+        int turnoEnPantalla=turnoActual;
+        turnoEnPantalla=26;
+
+        System.err.println("turno actual:"+ turnoActual);
+        
+        System.out.println("turno en pantalla:"+ turnoEnPantalla);
+
+    }
+    
+}
