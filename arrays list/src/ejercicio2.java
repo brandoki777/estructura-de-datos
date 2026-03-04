74ec35943bf47be623c98a0f8fa902984353cb6e
brandoki777 <bransanro4789@gmail.com>
2026-03-04 07:04:17 -0500

    2

--- arrays list/src/ejercicio2.java
+++ arrays list/src/ejercicio2.java
@@ -5,32 +5,32 @@
 //Algoritmo a usar: Búsqueda Binaria. Como los datos ya están ordenados, este algoritmo permitirá encontrar al cliente dividiendo la lista a la mitad en cada paso.
 //public class ejercicio2 {
 
-    public static String busquedaBinaria(int[] clientes, int idBuscado) {
+    public static String busqueda_Binaria(int[] clientes, int id) {
         int inicio = 0;
         int fin = clientes.length - 1;
 
         while (inicio <= fin) {
             int medio = (inicio + fin) / 2;
 
-            if (clientes[medio] == idBuscado) {
-                return "Cliente encontrado en la posición " + medio;
-            } else if (clientes[medio] < idBuscado) {
+            if (clientes[medio] == id) {
+                return " EL cliente ha sido encontrado en la posición N° " + medio;
+            } else if (clientes[medio] < id) {
                 inicio = medio + 1; 
             } else {
                 fin = medio - 1; 
             }
         }
-        return "Cliente no encontrado en la lista";
+        return "Cliente no se ha encontrado";
     }
 
     public static void main(String[] args) {
         
         int[] clientes = {1001, 1010, 1025, 1030, 1050, 1100, 1200};
 
-        int idBuscado = 1050; 
+        int id = 1050; 
 
         
-        String resultado = busquedaBinaria(clientes, idBuscado);
+        String resultado = busqueda_Binaria(clientes, id);
 
         
         System.out.println(resultado);
--- arrays list/src/ejercicio3.java
+++ arrays list/src/ejercicio3.java
@@ -1,20 +1,17 @@
 //Una máquina industrial registra la temperatura cada 10 minutos y guarda los datos en un arreglo.
-
 //El Problema: Al final del día, el supervisor quiere saber cuál fue la temperatura más alta registrada para asegurarse de que la máquina no se recalentó.
 //Algoritmo a usar: Búsqueda Lineal de Máximo. Debes recorrer todo el arreglo comparando cada valor para encontrar el mayor de todos.
-
-
 public class ejercicio3 {
 
     
-    public static int buscarMaximo(int[] temperaturas) {
-        int maximo = temperaturas[0];
+    public static int buscar_Max(int[] temperaturas) {
+        int max = temperaturas[0];
         for (int i = 1; i < temperaturas.length; i++) {
-            if (temperaturas[i] > maximo) {
-                maximo = temperaturas[i]; 
+            if (temperaturas[i] > max) {
+                max = temperaturas[i]; 
             }
         }
-        return maximo;
+        return max;
     }
 
     public static void main(String[] args) {
@@ -22,7 +19,7 @@ public class ejercicio3 {
         int[] temperaturas = {75, 80, 78, 85, 90, 88, 92, 87};
 
         
-        int maximo = buscarMaximo(temperaturas);
+        int maximo = buscar_Max(temperaturas);
 
        
         System.out.println("La temperatura más alta registrada fue: " + maximo + "°C");
